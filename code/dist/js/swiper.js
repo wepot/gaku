229413e76bad5eb45999a29c11765185faaf4d0b
marumaki <marumaki0222@gmail.com>
2021-09-20 03:52:04 +0900

    【アップロード】GAKU静的実装ファイル一式(9/20)

---
+++ code/dist/js/swiper.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var swiper1 = new Swiper(".topSwiper", {
+  speed: 1800,
+  // スライドが切り替わる時の速さ
+  slidesPerView: "1",
+  // スライド表示数
+  loop: true,
+  // ループさせる
+  spaceBetween: 22,
+  autoplay: {
+    // 自動再生 ON
+    delay: 8000,
+    // 次のスライドまでの秒数
+    disableOnInteraction: false //スライダー操作後、自動再生が止まるかどうか
+
+  },
+  pagination: {
+    el: ".topSwiper-pagination"
+  }
+});
+var swiper2 = new Swiper(".aboutSwiper", {
+  speed: 1800,
+  // スライドが切り替わる時の速さ
+  slidesPerView: "1",
+  // スライド表示数
+  loop: true,
+  // ループさせる
+  spaceBetween: 72,
+  autoplay: {
+    // 自動再生 ON
+    delay: 8000,
+    // 次のスライドまでの秒数
+    disableOnInteraction: false //スライダー操作後、自動再生が止まるかどうか
+
+  },
+  pagination: {
+    el: ".aboutSwiper-pagination"
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: "2",
+      spaceBetween: 20
+    },
+    750: {
+      slidesPerView: "3",
+      spaceBetween: 24
+    }
+  }
+});
---
+++ code/src/js/script.js
@@ -0,0 +1,69 @@
+//--------------------------------
+// ハンバーガーメニュー開閉
+//--------------------------------
+$(function () {
+  $(".gnavSp_btn").on("click", function () {
+    $(this).toggleClass("open");
+
+    if ($(this).hasClass("open")) {
+      $("body").addClass("is-fixed");
+      $(".gnavSp").addClass("open");
+    } else {
+      $("body").removeClass("is-fixed");
+      $(".gnavSp").removeClass("open");
+    }
+  });
+});
+
+//--------------------------------
+// 検索ボックス
+//--------------------------------
+$(".open-btn1").click(function () {
+  $(this).toggleClass("btn-active");
+  $("#search-wrap").toggleClass("panel-active");
+  $("#search-text").focus();
+});
+
+//--------------------------------
+// トップへ戻るボタン
+//--------------------------------
+function getScrolled() {
+  return ( window.pageYOffset !== undefined ) ? window.pageYOffset: document.documentElement.scrollTop;
+}
+var topButton = document.getElementById( 'js-scroll-fadein' );
+window.onscroll = function() {
+  ( getScrolled() > 200 ) ? topButton.classList.add( 'is-fadein' ): topButton.classList.remove( 'is-fadein' );
+};
+function scrollToTop() {
+  var scrolled = getScrolled();
+  window.scrollTo( 0, Math.floor( scrolled / 2 ) );
+  if ( scrolled > 0 ) {
+    window.setTimeout( scrollToTop, 30 );
+  }
+};
+topButton.onclick = function() {
+  scrollToTop();
+};
+
+//--------------------------------
+// 内部リンク位置
+//--------------------------------
+jQuery(function ($) {
+  var headerHeight = $("header").outerHeight();
+  var urlHash = location.hash;
+  if (urlHash) {
+    $("body,html").stop().scrollTop(0);
+    setTimeout(function () {
+      var target = $(urlHash);
+      var position = target.offset().top - headerHeight;
+      $("body,html").stop().animate({ scrollTop: position }, 500);
+    }, 100);
+  }
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href);
+    var position = target.offset().top - headerHeight;
+    $("body,html").stop().animate({ scrollTop: position }, 500);
+    return false;
+  });
+});
