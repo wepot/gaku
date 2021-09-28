5915db4701edd55d17dcc35c006fdb76c61adf43
marumaki <marumaki0222@gmail.com>
2021-09-29 02:46:34 +0900

    【PHP修正】ご利用案内：テキスト修正

--- wp/page-user-guide.php
+++ wp/page-user-guide.php
@@ -6,7 +6,7 @@
           <section id="guide" class="sec-guide01">
             <header class="section_heading">
               <h1 class="section_title"><img src="<?php echo get_template_directory_uri(); ?>/common/img/title/guide01.svg" width="257" height="101" alt="ご利用案内"></h1>
-            </header><!-- /.section_headding -->
+            </header><!-- /.section_heading -->
             <p class="guide-arrowTitle">利用料金</p>
               <ul class="sec-guide01_list">
                 <li class="price_item">
@@ -23,7 +23,7 @@
           <section class="sec-guide02">
             <header class="section_heading">
               <h2 class="section_title"><img src="<?php echo get_template_directory_uri(); ?>/common/img/title/guide02.svg" width="257" height="101" alt="音楽教室"></h2>
-            </header><!-- /.section_headding -->
+            </header><!-- /.section_heading -->
             <ul class="class">
               <li class="class_item">
                 <figure class="class_img">
@@ -75,7 +75,7 @@
           <section class="sec-guide03">
             <header class="section_heading">
               <h2 class="section_title"><img src="<?php echo get_template_directory_uri(); ?>/common/img/title/guide03.svg" width="257" height="101" alt="利用シーン"></h2>
-            </header><!-- /.section_headding -->
+            </header><!-- /.section_heading -->
             <p class="sec-guide03_caption">こんなシーンでご利用ください</p>
             <div class="sec-guide03_wrap">
               <ul class="sec-guide03_list">
@@ -85,15 +85,15 @@
                 </li>
                 <li class="sec-guide03_item">
                   <h3 class="sec-guide03_title">茶話会、<br>手作りクラフト会など</h3>
-                  <p class="sec-guide03_text">当サロンはもともとカフェとして営業をしておりました。そのためキッチン設備もありお茶会や手作りクラフト会など趣味を共有する場所としても最適です。ゆっくりとくつろげる雰囲気の中で有意義な時間をお過ごしください。</p>
+                  <p class="sec-guide03_text">樂～ＧＡＫＵはもともと喫茶店として営業をしておりました。そのためキッチン設備もありお茶会や手作りクラフト会など趣味を共有する場所としても最適です。ゆっくりとくつろげる雰囲気の中で有意義な時間をお過ごしください。</p>
                 </li>
                 <li class="sec-guide03_item">
                   <h3 class="sec-guide03_title">音楽発表会・写真の個展</h3>
-                  <p class="sec-guide03_text">当サロンは音楽のコンサートだけでなく朗読会やご自身の趣味の発表会の会場としてもご利用頂けます。最大30名が収容できる会場で趣味仲間やご自身の展覧会など様々なシーンでご利用ください。</p>
+                  <p class="sec-guide03_text">音楽のコンサートだけでなく朗読会やご自身の趣味の発表会の会場としてもご利用いただけます。最大30名が収容できる会場で趣味仲間やご自身の展覧会など様々なシーンでご利用ください。</p>
                 </li>
                 <li class="sec-guide03_item">
                   <h3 class="sec-guide03_title">スクールの会場として</h3>
-                  <p class="sec-guide03_text">当サロンにはピアノを完備しておりますので音楽教室の場としても利用可能です。その他にも手芸や絵画など様々なスクールを開く事が可能です。是非お気軽にご相談下さい。</p>
+                  <p class="sec-guide03_text">当サロンにはピアノを完備しておりますので音楽教室の場としても利用可能です。その他にも手芸や絵画など様々なスクールを開く事が可能です。是非お気軽にご相談ください。</p>
                 </li>
               </ul>
             </div><!-- /.price_wrap -->
@@ -102,11 +102,11 @@
           <section class="sec-guide04">
             <header class="section_heading">
               <h2 class="section_title"><img src="<?php echo get_template_directory_uri(); ?>/common/img/title/guide04.svg" width="257" height="101" alt="利用規約"></h2>
-            </header><!-- /.section_headding -->
+            </header><!-- /.section_heading -->
             <div class="guideNum_wrap">
               <ul class="guideNum_list">
                 <li class="guideNum_item"><span>1</span>調律、会場設営、後片付けなどは利用時間内にお願い致します。</li>
-                <li class="guideNum_item"><span>2</span>サロンの備品、施設等の破損の場合、現況復帰の実費を戴きます。</li>
+                <li class="guideNum_item"><span>2</span>サロンの備品、施設等の破損の場合、現況復帰の実費をお支払いいただきます。</li>
                 <li class="guideNum_item"><span>3</span>出演者の控え室はございませんのでご了承ください。</li>
               </ul>
             </div><!-- /.guideNum_wrap -->
@@ -115,14 +115,14 @@
           <section class="sec-guide05">
             <header class="section_heading">
               <h2 class="section_title"><img src="<?php echo get_template_directory_uri(); ?>/common/img/title/guide05.svg" width="257" height="101" alt="お申し込み方法"></h2>
-            </header><!-- /.section_headding -->
+            </header><!-- /.section_heading -->
             <div class="guideNum_wrap">
               <ul class="guideNum_list">
-                <li class="guideNum_item"><span class="num">1</span>電話、メールにてご予約ください。</li>
+                <li class="guideNum_item"><span class="num">1</span>お問い合わせフォームまたはメールにてご予約ください。</li>
                 <li class="guideNum_item"><span class="num">2</span>予約後10日以内に、予約金（利用料金の半額）をお支払い願います。</li>
                 <li class="guideNum_item"><span class="num">3</span>残金は当日精算となります。</li>
               </ul>
-              <p class="guide_caption">※お申込者都合によるキャンセルの場合、予約金はご返金できません。</p>
+              <p class="guide_caption">※お申込者様の都合によるキャンセルの場合、予約金は返金できません。</p>
             </div><!-- /.guideNum_wrap -->
           </section><!-- /.sec-guide05 -->
 <?php get_template_part( 'parts/contactBox' );?>
